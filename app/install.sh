fce7c65a26aacba5cc88ce58ec6f3fe15ab1ce60
Benjamin Cremer <bc@shopware.com>
2016-02-09 21:04:52 +0100

    WIP

--- app/install.sh
+++ app/install.sh
@@ -13,27 +13,21 @@ if [ -f $__DIR__/../.env ]; then
     source $__DIR__/../.env
 fi
 
-echo "Setup Database"
 $__DIR__/../bin/console sw:setup --steps=drop,create,setup
 
 if [ $IMPORT_DEMODATA = true ] ; then
-    echo "Import Demodata"
     $__DIR__/../bin/console sw:setup --steps=importDemodata
 fi
 
-echo "Setup Shop Configuration"
 $__DIR__/../bin/console sw:setup  --steps=setupShop --host="$SHOP_HOST" --path="$SHOP_PATH"
 
-echo "Import Snippets"
 $__DIR__/../bin/console sw:snippets:to:db --include-plugins
 
-echo "Init Themes"
 $__DIR__/../bin/console sw:theme:initialize
 
-echo "Disable Firstrun Wizard"
 $__DIR__/../bin/console sw:firstrunwizard:disable
 
-echo "Create admin user"
 $__DIR__/../bin/console sw:create:admin --name="$ADMIN_NAME" --email="$ADMIN_EMAIL" --username="$ADMIN_USERNAME" --password="$ADMIN_PASSWORD" -n
 
+$__DIR__/create_symlinks.sh
 
